14ec1638e7558b01f6bdb716511896799eea9d48
sunzhida <suenzhida@gmail.com>
2020-08-10 17:06:35 +0800

    Update removing node function on 2020-08-10 17:06:35

--- app/static/js/interaction.js
+++ app/static/js/interaction.js
@@ -204,7 +204,7 @@ function shareto() {
 // The following functions are used for updating the data structure
 
 function _addImage(input) {
-    console.log(input);
+    // console.log(input);
     let imageName = input.split('/')[3];
     $.ajax({
         type: "POST",
@@ -224,7 +224,7 @@ function _exploreImage(i) {
     let imgName = i.split(',')[0];
     let keyword = i.split(',')[1];
     let imgID = parseInt(i.split(',')[2]);
-    console.log(i);
+    // console.log(i);
     $.ajax({
         url: "/inquire/" + i,
         type: "get",
@@ -242,6 +242,9 @@ function _exploreImage(i) {
 
 function _removeImage(input) {
     console.log(input);
+    let curTree = imageTree.remove(input);
+    console.log(curTree);
+    drawTree(curTree);
 }
 
 // The interactive functions
@@ -252,7 +255,7 @@ function browseImage(input) {
     let image_url = res[0];
     let image_name = res[0].split('/');
     let image_id = res[1];
-    console.log(image_id);
+    // console.log(image_id);
 
     let popup_kw = document.getElementById('keywords_' + image_id);
     if (popup_kw.style.display === "none") {
@@ -270,7 +273,7 @@ function browseImage(input) {
 }
 
 function browseImageList(input) {
-    console.log(input);
+    // console.log(input);
     let res = input.split(',');
     let image_url = res[0];
     let image_name = res[0].split('/');
@@ -279,10 +282,10 @@ function browseImageList(input) {
     let rectWidth = 252, rectHeight = 40;
 
     let currentRoot = imageTree.get(parseInt(window_id));
-    console.log(currentRoot);
-    console.log();
+    // console.log(currentRoot);
+    // console.log();
     let currentImageList = imageTree.find(parseInt(window_id));
-    console.log(currentImageList);
+    // console.log(currentImageList);
 
     d3.select('#kwindow_'+ window_id).remove();
     d3.select('#kbutton_'+ window_id).remove();
@@ -321,13 +324,13 @@ function browseImageList(input) {
         .attr('class', 'btn btn-danger btn-sm')
         .attr('type', 'button')
         .attr('id', 'remove_' + window_id)
-        .attr('onclick', 'remove(' + window_id + ')')
+        .attr('onclick', '_removeImage(' + window_id + ')')
         .append('i')
         .attr('class', 'fas fa-trash-alt');
 }
 
 function drawTree(d) {
-    console.log(d);
+    // console.log(d);
     // remove text
     canvas.select('text').remove();
     // remove the whole content
@@ -386,7 +389,7 @@ function drawTree(d) {
 }
 
 function drawTreeNode(d, group, rectWidth, rectHeight) {
-    console.log(d);
+    // console.log(d);
     if (d['color']) {
         drawRect(group, d['color']['x'], d['color']['y'], d['color']['id'], rectWidth, rectHeight);
         drawWin(group, d['color']['x'], d['color']['y'], d['color']['id'], rectWidth, rectHeight, d['color']['images']);
@@ -481,271 +484,153 @@ function drawWin(c, x, y, i, w, h, input) {
 }
 
 function prevSlide(e) {
-    console.log(e);
+    // console.log(e);
     d3.select('#kwindow_'+ e).remove();
     d3.select('#kbutton_'+ e).remove();
 }
 
 function nextSlide(e) {
-    console.log(e);
+    // console.log(e);
     d3.select('#kwindow_'+ e).remove();
     d3.select('#kbutton_'+ e).remove();
 }
 
-function addSubImage(x, y, i, input) {
-    console.log(input);
-    let imageWidth = 252, imageHeight = 120;
-    let group = container.append("g")
-        .attr("transform", "translate("
-            + x + "," + y + ")")
-        .attr('id', 'image_' + i)
-        .classed('draggable', true);
-    group.append('rect')
-        .style('fill', '#95a5a6')
-        .style('fill-opacity', '0.2')
-        .style('stroke', '#7f8c8d')
-        .style('stroke-width', 3)
-        .attr('width', imageWidth)
-        .attr('height', imageHeight);
-    let window = group.append('foreignObject')
-        .attr('width', imageWidth)
-        .attr('height', imageHeight)
-        .append('xhtml:div')
-        .attr("id", "window_" + i)
-        .attr('class', 'carousel slide')
-        .attr('data-interval', 'false')
-        // .attr('data-ride', 'carousel')
-        .attr('xmlns', 'http://www.w3.org/1999/xhtml');
-    let subwindow = window.append('div')
-        .attr('class', 'carousel-inner');
-    for (let m in input) {
-        if (m === '0') {
-            subwindow.append('div')
-                .attr('class', 'carousel-item active')
-                .attr('data-interval', 'false')
-                .append('img')
-                .attr('src', '../static/img/' + input[m]['name'])
-                .attr("id", "boarding_" + i + "_" + m)
-                .attr('data-carousel-id', 'carousel_' + m)
-                .attr('width', input[m]['width'] / input[m]['height'] * imageHeight)
-                .attr('height', imageHeight)
-                .attr('onmouseup', 'browseImage("' + input[m]['name'] + ',' + i + '")');
-        } else {
-            subwindow.append('div')
-                .attr('class', 'carousel-item')
-                .attr('data-interval', 'false')
-                .append('img')
-                .attr('src', '../static/img/' + input[m]['name'])
-                .attr("id", "boarding_" + i + "_" + m)
-                .attr('data-carousel-id', 'carousel_' + m)
-                .attr('width', input[m]['width'] / input[m]['height'] * imageHeight)
-                .attr('height', imageHeight)
-                .attr('onmouseup', 'browseImage("' + input[m]['name'] + ',' + i + '")');
-        }
-    }
-    let leftcon = window.append('a')
-        .attr('class', 'carousel-control-prev')
-        .attr('href', "#window_" + i)
-        .attr('role', 'button')
-        .attr('data-slide', 'prev');
-    leftcon.append('span')
-        .attr('class', 'carousel-control-prev-icon')
-        .attr('aria-hidden', 'true');
-    leftcon.append('span')
-        .attr('class', 'sr-only')
-        .html('Previous');
-    let rightcon = window.append('a')
-        .attr('class', 'carousel-control-next')
-        .attr('href', "#window_" + i)
-        .attr('role', 'button')
-        .attr('data-slide', 'next');
-    rightcon.append('span')
-        .attr('class', 'carousel-control-next-icon')
-        .attr('aria-hidden', 'true');
-    rightcon.append('span')
-        .attr('class', 'sr-only')
-        .html('Next');
-    // window.append('img')
-    //     .attr('src', '../static/img/' + input[0]['name'])
-    //     .attr('width', input[0]['width'] / input[0]['height'] * imageHeight)
-    //     .attr('height', imageHeight)
-    //     .attr('onmouseup', 'browseImage("' + input[0]['name'] + ',' + i + '")')
-    //     .attr("id", "boarding_" + i);
-    // group.append("image")
-    //     .attr('href', '../static/img/' + input['name'])
-    //     .attr('width', input['width'] / input['height'] * imageHeight)
-    //     .attr('height', imageHeight)
-    //     .attr('onmouseup', 'browseImage("' + input['name'] + ',' + i + '")')
-    //     .attr("id", "boarding_" + i);
-    let buttons = group.append("foreignObject")
-        .attr('x', imageWidth - 30)
-        .attr('y', 0)
-        .attr('width', 30)
-        .attr('height', 30)
-        .append('xhtml:div')
-        .attr('xmlns', 'http://www.w3.org/1999/xhtml')
-        .attr('style', 'display: none;')
-        .attr('id', 'button_' + i);
-    buttons.append('button')
-        .attr('class', 'btn btn-danger btn-sm hide')
-        .attr('type', 'button')
-        .attr('id', 'remove_' + i)
-        .attr('onclick', 'remove(' + i + ')')
-        .append('i')
-        .attr('class', 'fas fa-trash-alt');
-    let keywords = group.append("foreignObject")
-        .attr('x', 0)
-        .attr('y', -40)
-        .attr('width', imageWidth)
-        .attr('height', 40)
-        .append('xhtml:div')
-        .attr('xmlns', 'http://www.w3.org/1999/xhtml')
-        .attr('style', 'display: none;')
-        .attr('id', 'keywords_' + i);
-    for (let w in input[0]['keywords']) {
-        keywords.append('span')
-            .attr('class', 'badge badge-warning mr-1 hide')
-            .attr('type', 'button')
-            .attr('onclick', '_exploreImage("' + input[0]['name'] + ',' + input[0]['keywords'][w] + ',' + i + '")')
-            .html(input[0]['keywords'][w]);
-    }
-}
 
-function inquire(i) {
-    let imgName = i.split(',')[0];
-    let keyword = i.split(',')[1];
-    let imgID = i.split(',')[2];
-    console.log(imgName, keyword, imgID);
-    let img = document.getElementById("boarding_" + imgID).getBBox();
-    // let img1 = document.getElementById("boarding_" + imgID).getBoundingClientRect();
-    console.log(img);
-    let recHeight = 220, sec = 80, imageHeight = 120, imageWidth = 252;
-
-    let g_id = "#image_" + imgID;
-    let x1 = img.x + img.width, y1 = img.y + img.height / 2;
-    let x2 = img.x + img.width + sec / 2;
-    let y_semantic = y1 - recHeight / 2 - sec;
-    let y_color = y1;
-    let y_shape = y1 + recHeight / 2 + sec;
-
-    // image
-
-    $.ajax({
-        url: "/inquire/" + i,
-        type: "get",
-        data: i,
-        success: function (response) {
-            let re = JSON.parse(response);
-            console.log(re);
-            // console.log(currentTree);
-            // console.log(x1, y_semantic, y_color, y_shape);
-            imageID += 1;
-            addSubImage(x1 + sec, y_semantic - imageHeight / 2, imageID, re['semantic']);
-            imageID += 1;
-            addSubImage(x1 + sec, y_color - imageHeight / 2, imageID, re['color']);
-            imageID += 1;
-            addSubImage(x1 + sec, y_shape - imageHeight / 2, imageID, re['shape']);
-        },
-        error: function (xhr) {
-            //Do Something to handle error
-        }
-    });
-
-    // line
-    let lineGenerator = d3.svg.line()
-        .x(function (d) {
-            return d.x;
-        })
-        .y(function (d) {
-            return d.y;
-        })
-        .interpolate('bundle');
-
-    console.log(x1, y_semantic, y_color, y_shape);
-
-    let path1 = [{'x': x1, 'y': y1}, {'x': x2, 'y': y1}, {'x': x1, 'y': y_semantic}, {'x': x2, 'y': y_semantic}];
-    let path2 = [{'x': x1, 'y': y1}, {'x': x2, 'y': y1}, {'x': x1, 'y': y_color}, {'x': x2, 'y': y_color}];
-    let path3 = [{'x': x1, 'y': y1}, {'x': x2, 'y': y1}, {'x': x1, 'y': y_shape}, {'x': x2, 'y': y_shape}];
-    let cen_x = (x1 + x2) / 2;
-    let cen_1 = (y1 + y_semantic) / 2;
-    let cen_2 = (y1 + y_color) / 2;
-    let cen_3 = (y1 + y_shape) / 2;
-
-    d3.select(g_id)
-        .append('path')
-        .attr('d', lineGenerator(path1))
-        .style('fill', 'none')
-        .style('stroke', '#e67e22')
-        .style('stroke-width', '3')
-        .on('mouseover', function () {
-            d3.select('#path_s_' + imgID)
-                .style('visibility', 'visible');
-        })
-        .on('mouseout', function () {
-            d3.select('#path_s_' + imgID)
-                .style('visibility', 'hidden');
-        });
-    d3.select(g_id)
-        .append('text')
-        .text('Semantic')
-        .attr('transform', function () {
-            return "translate(" + (cen_x - 30) + "," + (cen_1 - 11) + ")"
-        })
-        .attr('id', 'path_s_' + imgID)
-        .style('visibility', 'hidden')
-        .style('fill', '#d35400');
-
-    d3.select(g_id)
-        .append('path')
-        .attr('d', lineGenerator(path2))
-        .style('fill', 'none')
-        .style('stroke', '#9b59b6')
-        .style('stroke-width', '3')
-        .on('mouseover', function () {
-            d3.select('#path_c_' + imgID)
-                .style('visibility', 'visible');
-        })
-        .on('mouseout', function () {
-            d3.select('#path_c_' + imgID)
-                .style('visibility', 'hidden');
-        });
-    d3.select(g_id)
-        .append('text')
-        .text('Color')
-        .attr('transform', function () {
-            return "translate(" + (cen_x - 15) + "," + (cen_2 - 11) + ")"
-        })
-        .attr('id', 'path_c_' + imgID)
-        .style('visibility', 'hidden')
-        .style('fill', '#8e44ad');
-
-    d3.select(g_id)
-        .append('path')
-        .attr('d', lineGenerator(path3))
-        .style('fill', 'none')
-        .style('stroke', '#2ecc71')
-        .style('stroke-width', '3')
-        .on('mouseover', function (d) {
-            d3.select('#path_sh_' + imgID)
-                .style('visibility', 'visible');
-        })
-        .on('mouseout', function (d) {
-            d3.select('#path_sh_' + imgID)
-                .style('visibility', 'hidden');
-        })
-        .on('click', function (d) {
-            console.log(d);
-        });
-    d3.select(g_id)
-        .append('text')
-        .text('Shape')
-        .style('fill', '#27ae60')
-        .attr('id', 'path_sh_' + imgID)
-        .style('visibility', 'hidden')
-        .attr('transform', function (d) {
-            return "translate(" + (cen_x - 15) + "," + (cen_3 - 11) + ")"
-        });
-}
+// function inquire(i) {
+//     let imgName = i.split(',')[0];
+//     let keyword = i.split(',')[1];
+//     let imgID = i.split(',')[2];
+//     console.log(imgName, keyword, imgID);
+//     let img = document.getElementById("boarding_" + imgID).getBBox();
+//     // let img1 = document.getElementById("boarding_" + imgID).getBoundingClientRect();
+//     console.log(img);
+//     let recHeight = 220, sec = 80, imageHeight = 120, imageWidth = 252;
+//
+//     let g_id = "#image_" + imgID;
+//     let x1 = img.x + img.width, y1 = img.y + img.height / 2;
+//     let x2 = img.x + img.width + sec / 2;
+//     let y_semantic = y1 - recHeight / 2 - sec;
+//     let y_color = y1;
+//     let y_shape = y1 + recHeight / 2 + sec;
+//
+//     // image
+//
+//     $.ajax({
+//         url: "/inquire/" + i,
+//         type: "get",
+//         data: i,
+//         success: function (response) {
+//             let re = JSON.parse(response);
+//             console.log(re);
+//             // console.log(currentTree);
+//             // console.log(x1, y_semantic, y_color, y_shape);
+//             imageID += 1;
+//             addSubImage(x1 + sec, y_semantic - imageHeight / 2, imageID, re['semantic']);
+//             imageID += 1;
+//             addSubImage(x1 + sec, y_color - imageHeight / 2, imageID, re['color']);
+//             imageID += 1;
+//             addSubImage(x1 + sec, y_shape - imageHeight / 2, imageID, re['shape']);
+//         },
+//         error: function (xhr) {
+//             //Do Something to handle error
+//         }
+//     });
+//
+//     // line
+//     let lineGenerator = d3.svg.line()
+//         .x(function (d) {
+//             return d.x;
+//         })
+//         .y(function (d) {
+//             return d.y;
+//         })
+//         .interpolate('bundle');
+//
+//     console.log(x1, y_semantic, y_color, y_shape);
+//
+//     let path1 = [{'x': x1, 'y': y1}, {'x': x2, 'y': y1}, {'x': x1, 'y': y_semantic}, {'x': x2, 'y': y_semantic}];
+//     let path2 = [{'x': x1, 'y': y1}, {'x': x2, 'y': y1}, {'x': x1, 'y': y_color}, {'x': x2, 'y': y_color}];
+//     let path3 = [{'x': x1, 'y': y1}, {'x': x2, 'y': y1}, {'x': x1, 'y': y_shape}, {'x': x2, 'y': y_shape}];
+//     let cen_x = (x1 + x2) / 2;
+//     let cen_1 = (y1 + y_semantic) / 2;
+//     let cen_2 = (y1 + y_color) / 2;
+//     let cen_3 = (y1 + y_shape) / 2;
+//
+//     d3.select(g_id)
+//         .append('path')
+//         .attr('d', lineGenerator(path1))
+//         .style('fill', 'none')
+//         .style('stroke', '#e67e22')
+//         .style('stroke-width', '3')
+//         .on('mouseover', function () {
+//             d3.select('#path_s_' + imgID)
+//                 .style('visibility', 'visible');
+//         })
+//         .on('mouseout', function () {
+//             d3.select('#path_s_' + imgID)
+//                 .style('visibility', 'hidden');
+//         });
+//     d3.select(g_id)
+//         .append('text')
+//         .text('Semantic')
+//         .attr('transform', function () {
+//             return "translate(" + (cen_x - 30) + "," + (cen_1 - 11) + ")"
+//         })
+//         .attr('id', 'path_s_' + imgID)
+//         .style('visibility', 'hidden')
+//         .style('fill', '#d35400');
+//
+//     d3.select(g_id)
+//         .append('path')
+//         .attr('d', lineGenerator(path2))
+//         .style('fill', 'none')
+//         .style('stroke', '#9b59b6')
+//         .style('stroke-width', '3')
+//         .on('mouseover', function () {
+//             d3.select('#path_c_' + imgID)
+//                 .style('visibility', 'visible');
+//         })
+//         .on('mouseout', function () {
+//             d3.select('#path_c_' + imgID)
+//                 .style('visibility', 'hidden');
+//         });
+//     d3.select(g_id)
+//         .append('text')
+//         .text('Color')
+//         .attr('transform', function () {
+//             return "translate(" + (cen_x - 15) + "," + (cen_2 - 11) + ")"
+//         })
+//         .attr('id', 'path_c_' + imgID)
+//         .style('visibility', 'hidden')
+//         .style('fill', '#8e44ad');
+//
+//     d3.select(g_id)
+//         .append('path')
+//         .attr('d', lineGenerator(path3))
+//         .style('fill', 'none')
+//         .style('stroke', '#2ecc71')
+//         .style('stroke-width', '3')
+//         .on('mouseover', function (d) {
+//             d3.select('#path_sh_' + imgID)
+//                 .style('visibility', 'visible');
+//         })
+//         .on('mouseout', function (d) {
+//             d3.select('#path_sh_' + imgID)
+//                 .style('visibility', 'hidden');
+//         })
+//         .on('click', function (d) {
+//             console.log(d);
+//         });
+//     d3.select(g_id)
+//         .append('text')
+//         .text('Shape')
+//         .style('fill', '#27ae60')
+//         .attr('id', 'path_sh_' + imgID)
+//         .style('visibility', 'hidden')
+//         .attr('transform', function (d) {
+//             return "translate(" + (cen_x - 15) + "," + (cen_3 - 11) + ")"
+//         });
+// }
 
 function remove(i) {
     let g_id = "#image_" + i;

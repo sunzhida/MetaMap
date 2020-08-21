3539aeeee491772ebe20e7cc25603cc526a2fd21
Andy_KANG <30693282+AndyKANG98@users.noreply.github.com>
2020-08-22 01:25:48 +0800

    更改fake color_palette为本机测试

--- app/__init__.py
+++ app/__init__.py
@@ -305,7 +305,16 @@ def search(i):
     conn = create_connection(DATABASE)
     keyword = suggestKeyword(conn, i)
     image = searchImage(conn, i)
-    color_palette = colorPalette(conn, i)
+    # color_palette = colorPalette(conn, i)
+    color_palette = [
+        {'color':"#1F77B4", 'portion':0.3},
+        {'color':"#FF7F0E", 'portion':0.2},
+        {'color':"#2CA02C", 'portion':0.2},
+        {'color':"#D62728", 'portion':0.1},
+        {'color':"#9467BD", 'portion':0.1},
+        {'color':"#8C564B", 'portion':0.1},
+    ]
+
     data = {'search': i, 'keywords': keyword, 'images': image, 'colors': color_palette}
     conn.close()
     return json.dumps(data)
